b365248b6a6e32a7c01cf0cfa29a07967305cf29
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2025-09-14 22:36:18 +0300

    Update PageTitle component title and adjust Hero component SVG opacity; add new favicon SVG

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,6 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 const PageTitle = ({ title }) => {
-  const defaultTitle = 'Five Software Soultions'
+  const defaultTitle = 'Acwad - Software Solutions & IT Services'
   return (
     <Helmet>
       <title>{title ? title + ' | ' + defaultTitle : defaultTitle}</title>
